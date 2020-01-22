d47ebb39ff516295324044d62538908e863f171a
Dalvin Cash <cashdalvin1@gmail.com>
2020-01-22 10:40:38 -0600

    npm-exercise

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const $ = require('jquery');
+const sayHello = () => console.log('hello');
+$('body').css('background-color', 'green').append('<h1>Hello12</h1>');
+
+
+console.log('hello2');
+console.log(sayHello());
+
+
+// $('#div1').after('<div style="background-color: red">New div</div>');
